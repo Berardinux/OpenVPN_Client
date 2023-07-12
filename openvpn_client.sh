a2dbbf6de687d4fee4e6ca421d53f1ab4f7ad871
Berardinux <113406827+Berardinux@users.noreply.github.com>
2023-07-12 15:46:48 -0400

    Update openvpn_client.sh

--- openvpn_client.sh
+++ openvpn_client.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-echo "Are you ready to install OpenVPN? (Y/N)"
+echo "# Are you ready to install OpenVPN? (Y/N) #"
 read -r one
 
 if [ "$one" == "Y" ]; then
     sudo apt update && sudo apt upgrade -y && sudo apt install openvpn -y
 else
-    echo "Okay, run the script again when you are ready to install!"
+    echo "# Okay, run the script again when you are ready to install! #"
     exit
 fi
 
 clear
 
-echo "What is your .ovpn file called?"
-echo "Remember to include the .ovpn at the end of the file!!"
+echo "########## What is your .ovpn file called? ###############"
+echo "# Remember to include the .ovpn at the end of the file!! #"
 read -r ovpn
 clear
 
-echo "Where is your $ovpn file located?"
+echo "###### Where is your $ovpn file located? ##########"
+echo "# (PS) Just the path to the directory $ovpn is in #"
+echo "# dont include $ovpn in the path to the directory #"
 read -r location
 
 sudo mv "$location/$ovpn" "/etc/openvpn/client/$ovpn"
 clear
 
-echo "What would you like to call your OpenVPN command?"
+echo "# What would you like to call your OpenVPN command? #"
 read -r command
 clear
 
@@ -33,11 +35,11 @@ sudo chmod 777 /usr/local/bin/$command
 echo '#!/bin/bash' | sudo tee "/usr/local/bin/$command" >/dev/null
 echo "sudo openvpn /etc/openvpn/client/$ovpn" | sudo tee -a "/usr/local/bin/$command" >/dev/null
 
-echo "What would you like the permissions to be for the .ovpn file? (Suggested = 660)"
-echo "Read permission: Numeric value of 4"
-echo "Write permission: Numeric value of 2"
-echo "Execute permission: Numeric value of 1"
-echo "Press (Enter) to use suggested permissions"
+echo "# What would you like the permissions to be for the .ovpn file? (Suggested = 660) #"
+echo "#################### Read permission: Numeric value of 4 ##########################"
+echo "################### Write permission: Numeric value of 2 ##########################"
+echo "################### Execute permission: Numeric value of 1 ########################"
+echo "############### Press (Enter) to use suggested permissions ########################"
 read -r mod
 
 if [ -z "$mod" ]; then
@@ -48,4 +50,4 @@ fi
 
 clear
 
-echo "To Connect to the VPN run the command $command"
+echo "###### To Connect to the VPN run the command $command ######"
