4acd6e4f593b43e3153f3ea024efbe2df12e2ca4
Berardinux <berardinux@gmail.com>
2023-07-19 12:57:49 -0400

    Got it working again

--- Install.sh
+++ Install.sh
@@ -2,7 +2,7 @@
 
 clear
 distro=$(grep -w "NAME" /etc/os-release | cut -d= -f2 | tr -d '"' | cut -d' ' -f1)
-location=$(find /path/to/search/directory -type f -name "*.ovpn" 2>/dev/null)
+location=$(find / -type f -name "*.ovpn" 2>/dev/null)
 
 if [ "$distro" = "Debian" ] || [ "$distro" = "Ubuntu" ]; then
     if dpkg -l | grep -q "openvpn"; then
@@ -19,14 +19,14 @@ elif [ "$distro" = "Arch" ]; then
         echo "OpenVPN is Not installed on your system yet"
     fi
 else
-    echo "Your Distro can not be install useding this install script sorry!"
-    echo "Distro's that can be install include ( Debian / Ubuntu / Arch )"
+    echo "Your Distro can not be installed using this install script sorry!"
+    echo "Distros that can be installed include (Debian / Ubuntu / Arch)"
     exit 1
 fi
 
 clear
 
-if [ -z "$location" ];
+if [ -z "$location" ]; then
     echo "Not able to find .ovpn file try again when you have one"
     exit 1
 else
@@ -37,11 +37,11 @@ else
     if [ "$answer" = "Y" ]; then
         echo "Okay Cool!"
     elif [ "$answer" = "N" ]; then
-        echo "Okay what is you .ovpn file called including the .ovpn at the end?"
+        echo "Okay what is your .ovpn file called, including the .ovpn at the end?"
         read -r ovpn
-        location=$(sudo find / -name $ovpn 2>/dev/null)
+        location=$(sudo find / -name "$ovpn" 2>/dev/null)
         if [ -z "$location" ]; then
-            echo "Your .ovpn was not found try again later."
+            echo "Your .ovpn was not found. Try again later."
             exit 1
         fi
     fi
@@ -73,19 +73,19 @@ else
     exit
 fi
 
-    location=$(sudo find / -name $ovpn 2>/dev/null)
+location=$(sudo find / -name "$ovpn" 2>/dev/null)
 sudo mv "$location" "/etc/openvpn/client/$ovpn"
 sudo chmod 660 "/etc/openvpn/client/$ovpn"
 clear
 
 sudo touch "/usr/local/bin/$command"
-sudo chmod 777 /usr/local/bin/$command
+sudo chmod 777 "/usr/local/bin/$command"
 cat << EOM | sudo tee -a "/usr/local/bin/$command" >/dev/null
 #!/bin/bash
 
 start_vpn() {
     echo "Starting OpenVPN..."
-    nohup sudo openvpn /etc/openvpn/client/$ovpn > /dev/null 2>&1 &
+    nohup sudo openvpn "/etc/openvpn/client/$ovpn" > /dev/null 2>&1 &
     echo "OpenVPN started. You can now close the terminal."
     echo "The command to stop the VPN is {$command stop}"
 }
@@ -109,37 +109,37 @@ case "\$1" in
         stop_vpn
         ;;
     *)
-        echo "Unknown option: $1"
-        echo "Usage: $0 {start/stop}"
+        echo "Unknown option: \$1"
+        echo "Usage: \$0 {start/stop}"
         exit 1
         ;;
 esac
 EOM
 
-if [ " app" = "Y" ]; then
-	echo "Installing OpenVPN to system!"
+if [ "$app" = "Y" ]; then
+    echo "Installing OpenVPN to system!"
 else
-	echo "###### To Connect to the VPN run the command $command ######"
-	exit 1
+    echo "###### To Connect to the VPN run the command $command ######"
+    exit 1
 fi
 
-sudo mkdir /opt/OpenVPN
-sudo mv/Images /opt/OpenVPN
+sudo mkdir -p /opt/OpenVPN
+sudo mv Images /opt/OpenVPN
 sudo touch /opt/OpenVPN/ovpn_app.conf
-sudo touch /home/$USER/.local/share/applications/OpenVPN.desktop
-sudo chmod 777 OpenVPN.py
-sudo chmod 777 /home/$USER/.local/share/applications/OpenVPN.desktop
+sudo touch "/home/$USER/.local/share/applications/OpenVPN.desktop"
+sudo chmod 777 /opt/OpenVPN/OpenVPN.py
+sudo chmod 777 "/home/$USER/.local/share/applications/OpenVPN.desktop"
 sudo mv OpenVPN.py /opt/OpenVPN
 
 cat << EOM | sudo tee -a "/opt/OpenVPN/ovpn_app.conf" >/dev/null
 ## ovpn_app.conf
 
 The name of the .ovpn files ( 
-	$ovpn
+    $ovpn
 )
 
 The name of the Commands (
-	$command
+    $command
 )
 EOM
 
@@ -152,4 +152,6 @@ Icon=/opt/OpenVPN/Images/openvpn.png
 Exec=/opt/OpenVPN/OpenVPN.py
 Terminal=false
 EOM
+
 echo "###### To Connect to the VPN run the command $command ######"
+
