3da7259d941f6e7ca9f90c6732d6d94b99abb87c
Berardinux <berardinux@gmail.com>
2023-07-19 15:55:34 -0400

    Update

--- Install.sh
+++ Install.sh
@@ -3,18 +3,29 @@
 clear
 distro=$(grep -w "NAME" /etc/os-release | cut -d= -f2 | tr -d '"' | cut -d' ' -f1)
 location=$(find / -type f -name "*.ovpn" 2>/dev/null)
+command=$(echo "$location" | rev | cut -d'/' -f1 | rev | cut -d'.' -f1)
+
+if [ $(id -u) -eq 0 ]; then
+	current_user=$SUDO_USER
+else
+	current_user=$USER
+fi
 
 if [ "$distro" = "Debian" ] || [ "$distro" = "Ubuntu" ]; then
     if dpkg -l | grep -q "openvpn"; then
-        echo "openvpn is installed on your system already"
-        exit 1
+        echo "openvpn is aready installed on your system already"
+        sudo chmod 777 scripts/Uninstall.sh
+	sudo ./scripts/Uninstall.sh
+	exit 1
     else
         echo "OpenVPN is Not installed on your system yet"
     fi
 elif [ "$distro" = "Arch" ]; then
-    if pacman -Q | grep -q "openvpn"; then
+    if pacman -Q openvpn >/dev/null 2>&1; then
         echo "OpenVPN is installed on your system already"
-        exit 1
+	sudo chmod 777 scripts/Uninstall.sh
+        sudo ./scripts/Uninstall.sh
+	exit 1
     else
         echo "OpenVPN is Not installed on your system yet"
     fi
@@ -30,44 +41,45 @@ if [ -z "$location" ]; then
     echo "Not able to find .ovpn file try again when you have one"
     exit 1
 else
-    echo "Is this your .ovpn file? (Y/N)"
+    echo "Is this your .ovpn file? (Y/n)"
     echo "$location"
     read -r answer
     clear
-    if [ "$answer" = "Y" ]; then
-        echo "Okay Cool!"
-    elif [ "$answer" = "N" ]; then
-        echo "Okay what is your .ovpn file called, including the .ovpn at the end?"
-        read -r ovpn
-        location=$(sudo find / -name "$ovpn" 2>/dev/null)
-        if [ -z "$location" ]; then
-            echo "Your .ovpn was not found. Try again later."
-            exit 1
+    if [ -z $answer ]; then
+        anser=Y
+        if [ "$answer" = "Y" ]; then
+            echo "Okay Cool!"
+        elif [ "$answer" = "n" ]; then
+            echo "Okay what is your .ovpn file called, including the .ovpn at the end?"
+            read -r ovpn
+            location=$(sudo find / -name "$ovpn" 2>/dev/null)
+            if [ -z "$location" ]; then
+                echo "Your .ovpn was not found. Try again later."
+                exit 1
+            fi
         fi
     fi
 fi
 
-echo "# Are you ready to install OpenVPN? (Y/N) #"
+echo "# Are you ready to install OpenVPN? (Y/n) #"
 read -r install
 clear
 
-echo "# Would you like to install the OpenVPN GUI APP? (Y/N)"
+echo "# Would you like to install the OpenVPN GUI APP? (Y/n)"
 read -r app
 clear
 
-echo "# What would you like to call your OpenVPN command? #"
-read -r command
-clear
-
-if [ "$install" = "Y" ]; then
-    if [ "$distro" = "Debian" ] || [ "$distro" = "Ubuntu" ]; then
-        sudo apt update && sudo apt upgrade -y && sudo apt install openvpn -y
-    elif [ "$distro" = "Arch" ]; then
-        sudo pacman -Syu && sudo pacman -S openvpn
-    else
-        exit
+if [ -z $install ]; then
+    install=Y
+    if [ "$install" = "Y" ]; then
+        if [ "$distro" = "Debian" ] || [ "$distro" = "Ubuntu" ]; then
+            sudo apt update && sudo apt upgrade -y && sudo apt install openvpn -y
+        elif [ "$distro" = "Arch" ]; then
+            sudo pacman -Syu && sudo pacman -S --noconfirm openvpn
+        else
+            exit
+        fi
     fi
-
 else
     echo "# Okay, run the script again when you are ready to install! #"
     exit
@@ -115,43 +127,39 @@ case "\$1" in
         ;;
 esac
 EOM
-
-if [ "$app" = "Y" ]; then
-    echo "Installing OpenVPN to system!"
-else
-    echo "###### To Connect to the VPN run the command $command ######"
-    exit 1
+if [ -z "app" ]; then
+    app=y
+    if [ "$app" = "Y" ]; then
+        echo "Installing OpenVPN to system!"
+    else
+        echo "###### To Connect to the VPN run the command $command ######"
+        exit 1
+    fi
 fi
 
 sudo mkdir -p /opt/OpenVPN
-sudo mv Images /opt/OpenVPN
+sudo cp -r * /opt/OpenVPN
 sudo touch /opt/OpenVPN/ovpn_app.conf
-sudo touch "/home/$USER/.local/share/applications/OpenVPN.desktop"
-sudo chmod 777 /opt/OpenVPN/OpenVPN.py
-sudo chmod 777 "/home/$USER/.local/share/applications/OpenVPN.desktop"
-sudo mv OpenVPN.py /opt/OpenVPN
+touch "/home/$current_user/.local/share/applications/OpenVPN.desktop"
+sudo chmod -R 777 /opt/OpenVPN/
+chmod 777 "/home/$current_user/.local/share/applications/OpenVPN.desktop"
 
 cat << EOM | sudo tee -a "/opt/OpenVPN/ovpn_app.conf" >/dev/null
 ## ovpn_app.conf
-
-The name of the .ovpn files ( 
-    $ovpn
-)
-
-The name of the Commands (
-    $command
+## The name of the Commands (
+$command
 )
 EOM
 
-cat << EOM | sudo tee -a "/home/$USER/.local/share/applications/OpenVPN.desktop" >/dev/null
+cat << EOM | sudo tee -a "/home/$current_user/.local/share/applications/OpenVPN.desktop" >/dev/null
 [Desktop Entry]
 Type=Application
 Name=OpenVPN
 Comment=OpenVPN
 Icon=/opt/OpenVPN/Images/openvpn.png
-Exec=/opt/OpenVPN/OpenVPN.py
+Exec=/opt/OpenVPN/OpenVPN
 Terminal=false
 EOM
 
-echo "###### To Connect to the VPN run the command $command ######"
+echo "###### To Connect to the VPN run the command $command start ######"
 
---
+++ scripts/Uninstall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+distro=$(grep -w "NAME" /etc/os-release | cut -d= -f2 | tr -d '"' | cut -d' ' -f1)
+desktop=$(find / -name "OpenVPN.desktop" 2>/dev/null)
+command=$(sed -n '3p' /opt/OpenVPN/ovpn_app.conf)
+
+echo "Would you like to uninstall OpenVPN? (Y/n)"
+read -r uninstall
+
+if [ -z "$uninstall" ]; then
+	uninstall=Y
+	if [ "$uninstall" = "Y" ]; then
+		if [ "$distro" = "Debian" ] || [ "$distro" = "Ubuntu" ]; then
+    			sudo apt purge openvpn
+
+		elif [ "$distro" = "Arch" ]; then
+			sudo pacman -Rns openvpn
+		else
+			echo "This Uninstall script will not work on you Distro."
+			exit 1
+		fi
+	fi
+elif [ "$uninstall" = "N" ]; then
+	echo "Okay then."
+	exit 1
+else
+	echo "That was not a option try again later."
+	exit 1
+fi
+
+sudo rm $desktop
+sudo rm /usr/local/bin/$command
+sudo rm -r /etc/openvpn
+sudo rm -r /opt/OpenVPN
